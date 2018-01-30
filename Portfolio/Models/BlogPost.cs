43e188bf86ad54f267b735c635789a68e14a7647
Joseph <bizzclaw1@gmail.com>
2018-01-30 10:03:12 -0800

    Add models for PageInfo and BlogPost

---
+++ Portfolio/Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Portfolio.Models
+{
+    [Table(DbTable)]
+    public class BlogPost
+    {
+        public const string DbTable = "blogposts";
+
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Body { get; set; }
+    }
+}
---
+++ Portfolio/Models/PageInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Portfolio.Models
+{
+    [Table(DbTable)]
+    public class PageInfo
+    {
+        public const string DbTable = "pageinfo";
+        public string Title { get; set; }
+        [Column(TypeName = "ntext")]
+        public string MainInfo { get; set; }
+    }
+}
